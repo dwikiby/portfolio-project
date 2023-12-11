d4748e47549b839f4d95ddc135949a8ce33b170c
Dwiki Ramadhan <dwikiakbarramadhan@getslurp.com>
2023-12-11 21:36:25 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Portfolio Project
+
+## Overview
+
+This repository contains the source code for my personal portfolio website, built using Next.js and styled with Tailwind CSS. The project aims to showcase my skills, projects, and experience in a visually appealing and interactive manner.
+
+## Technologies Used
+
+- **Next.js:** The project is built on the Next.js framework, providing a fast and efficient way to create React applications with server-side rendering and other performance optimizations.
+
+- **Tailwind CSS:** The styling is done using Tailwind CSS, a utility-first CSS framework that enables rapid development and easy customization.
+
+## Getting Started
+
+### Prerequisites
+
+Before you begin, ensure you have the following tools installed:
+
+- Node.js
+- npm or pnpm (Note: This project recommends using pnpm for package management.)
---
+++ lib/data.ts
@@ -0,0 +1,107 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+import avatarCraft from "@/public/avatar-ai.png";
+import cloudGo from "@/public/cloud-go.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Graduated bootcamp",
+    location: "Miami, FL",
+    description:
+      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2019",
+  },
+  {
+    title: "Front-End Developer",
+    location: "Orlando, FL",
+    description:
+      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2019 - 2021",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "Houston, TX",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "2021 - present",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Avatar Craft AI",
+    description:
+      "This is my personal work with Next.js, this project using replicate dreambooth to generated avatar from training models.",
+    tags: ["React", "Next.js", "PostGres", "Tailwind", "Prisma"],
+    imageUrl: avatarCraft,
+  },
+  {
+    title: "Cloud Getorders",
+    description:
+      "This is a full-time project in GetOrders, currently working to maintain this website from managing data in the backend",
+    tags: ["Svelte", "JavaScript", "CSS", "MongoDB", "NodeJs"],
+    imageUrl: cloudGo,
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: wordanalyticsImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Svelte",
+  "Vue.js",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "Rest API",
+  "Nest.js",
+  "Express",
+  "PostgreSQL",
+] as const;
